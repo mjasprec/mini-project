57ec22d69f755f868173895fd1559da3327ea7f5
MJAsprec <michael.asprec@whitecloak.com>
2024-04-08 16:10:29 +0800

    update: signup modal

--- clients/user-ui/src/screens/AuthScreen.tsx
+++ clients/user-ui/src/screens/AuthScreen.tsx
@@ -7,7 +7,7 @@ function AuthScreen() {
   const [activeState, setActiveState] = useState('sign-in');
   return (
     <div className='w-full h-screen fixed top-0 left-0 z-50 flex items-center justify-center bg-[#0000003a]'>
-      <div className='w-[35%] bg-slate-900 rounded shadow-sm p-5'>
+      <div className='w-[38%] h-auto bg-slate-900 rounded shadow-sm p-5'>
         {activeState === 'sign-in' ? (
           <SignIn setActiveState={setActiveState} />
         ) : (
--- clients/user-ui/src/shared/Auth/SignUp.tsx
+++ clients/user-ui/src/shared/Auth/SignUp.tsx
@@ -1,25 +1,283 @@
 'use client';
 import styles from '@/utils/styles';
-import React, { Dispatch, SetStateAction } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 import { z } from 'zod';
 
-const signUpFormSchema = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
+const GENDER = ['male', 'female'] as const;
+
+const formSchema = z.object({
+  firstName: z.string().min(2, 'First name must be at least 2 characters'),
+  lastName: z.string().min(2, 'Last name must be at least 2 characters'),
   email: z.string().email(),
   username: z.string().min(6, 'Username must be at least 6 characters'),
   password: z.string().min(6, 'Username must be at least 6 characters'),
+  about: z.string().max(64, 'About cannot exceed 64 characters'),
+  gender: z.enum(GENDER),
+  birthday: z.coerce.date(),
+  wallet: z.coerce.string(),
 });
+
+type SignUpFormSchema = z.infer<typeof formSchema>;
 
 type SignUpPropType = {
   setActiveState: Dispatch<SetStateAction<string>>;
 };
 
 function SignUp({ setActiveState }: SignUpPropType) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<SignUpFormSchema>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (data: SignUpFormSchema) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <div>
       <h1 className={`${styles.title}`}>Sign Up</h1>
-      <form>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='flex flex-row gap-4'>
+          <div className='w-full mt-3 relative mb-1'>
+            <label
+              htmlFor='firstName'
+              className={`${styles.label}`}
+            >
+              First Name
+            </label>
+            <input
+              {...register('firstName')}
+              type='text'
+              placeholder='First Name'
+              className={`${styles.input}`}
+            />
+            {errors.firstName ? (
+              <span className={`text-red-500 block mt-1`}>
+                {errors.firstName.message}
+              </span>
+            ) : null}
+          </div>
+          <div className='w-full mt-3 relative mb-1'>
+            <label
+              htmlFor='lastName'
+              className={`${styles.label}`}
+            >
+              Last Name
+            </label>
+            <input
+              {...register('lastName')}
+              type='text'
+              placeholder='Last Name'
+              className={`${styles.input}`}
+            />
+            {errors.lastName ? (
+              <span className={`text-red-500 block mt-1`}>
+                {errors.lastName.message}
+              </span>
+            ) : null}
+          </div>
+        </div>
+
+        <div className='w-full mt-3 relative mb-1'>
+          <label
+            htmlFor='email'
+            className={`${styles.label}`}
+          >
+            Email
+          </label>
+          <input
+            {...register('email')}
+            type='email'
+            placeholder='email@email.com'
+            className={`${styles.input}`}
+          />
+          {errors.email ? (
+            <span className={`text-red-500 block mt-1`}>
+              {errors.email.message}
+            </span>
+          ) : null}
+        </div>
+
+        <div className='flex flex-row gap-4'>
+          <div className='w-full mt-3 relative mb-1'>
+            <label
+              htmlFor='username'
+              className={`${styles.label}`}
+            >
+              Username
+            </label>
+            <input
+              {...register('username')}
+              type='text'
+              placeholder='username'
+              className={`${styles.input}`}
+            />
+            {errors.username ? (
+              <span className={`text-red-500 block mt-1`}>
+                {errors.username.message}
+              </span>
+            ) : null}
+          </div>
+
+          <div className='w-full mt-3 relative mb-1'>
+            <label
+              htmlFor='password'
+              className={`${styles.label}`}
+            >
+              Password
+            </label>
+            <input
+              {...register('password')}
+              type={!showPassword ? 'password' : 'text'}
+              placeholder='password'
+              className={`${styles.input}`}
+            />
+            {errors.password ? (
+              <span className={`text-red-500 block mt-1`}>
+                {errors.password.message}
+              </span>
+            ) : null}
+
+            {!showPassword ? (
+              <AiOutlineEyeInvisible
+                className='absolute bottom-3 right-2 z-10 cursor-pointer'
+                size={20}
+                onClick={() => setShowPassword(true)}
+              />
+            ) : (
+              <AiOutlineEye
+                className='absolute bottom-3 right-2 z-10 cursor-pointer'
+                size={20}
+                onClick={() => setShowPassword(false)}
+              />
+            )}
+          </div>
+        </div>
+
+        <div className='w-full mt-3 relative mb-1'>
+          <label
+            htmlFor='about'
+            className={`${styles.label}`}
+          >
+            About
+          </label>
+
+          <textarea
+            {...register('about')}
+            name='about'
+            cols={30}
+            rows={30}
+            className={`${styles.input} p-1 h-[80px]`}
+          ></textarea>
+
+          {errors.about ? (
+            <span className={`text-red-500 block mt-1`}>
+              {errors.about.message}
+            </span>
+          ) : null}
+        </div>
+
+        <div className='flex flex-row gap-4'>
+          <div className='flex flex-col gap-5 w-full mt-3 relative mb-1'>
+            <label
+              htmlFor='gender'
+              className={`${styles.label}`}
+            >
+              Gender
+            </label>
+            <div className='w-full flex flex-row gap-3   mb-1 '>
+              <div>
+                <input
+                  type='radio'
+                  name='gender'
+                  value='male'
+                  checked
+                />
+                <label
+                  htmlFor='male'
+                  className='ml-2 inline-block'
+                >
+                  Male
+                </label>
+              </div>
+
+              <div>
+                <input
+                  type='radio'
+                  name='gender'
+                  value='female'
+                />
+                <label
+                  htmlFor='female'
+                  className='ml-2 inline-block'
+                >
+                  Female
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div className='w-full mt-3 relative mb-1'>
+            <label
+              htmlFor='birthday'
+              className={`${styles.label}`}
+            >
+              Birthday
+            </label>
+            <input
+              {...register('birthday')}
+              type='date'
+              placeholder='birthday'
+              className={`${styles.input}`}
+            />
+            {errors.birthday ? (
+              <span className={`text-red-500 block mt-1`}>
+                {errors.birthday.message}
+              </span>
+            ) : null}
+          </div>
+        </div>
+
+        <div className='w-full mt-3 relative mb-1'>
+          <label
+            htmlFor='wallet'
+            className={`${styles.label}`}
+          >
+            Wallet
+          </label>
+          <input
+            {...register('wallet')}
+            type='number'
+            name='wallet'
+            className={`${styles.input}`}
+          />
+          {errors.wallet ? (
+            <span className={`text-red-500 block mt-1`}>
+              {errors.wallet.message}
+            </span>
+          ) : null}
+        </div>
+        <div className='w-full mt-3'>
+          <input
+            type='submit'
+            value='Sign Up'
+            disabled={isSubmitting}
+            className={`${styles.button} mt-3`}
+            onClick={() => alert('Sign Up')}
+          />
+        </div>
+
         <h5 className='text-center pt-4 font-Poppins text-[14px] text-white'>
           Already have an account?
           <span
